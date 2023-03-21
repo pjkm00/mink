059885e8bafe5f97d0de3128db24588e38e8fd4a
pjkm00 <pjkm00@gmail.com>
2023-03-21 17:22:30 +0900

    .

--- 20230317/logIn.js
+++ 20230317/logIn.js
@@ -4,14 +4,22 @@ const h1 = document.querySelector("#greeting")
 
 const callback = function(e) {
     e.preventDefault();
-    localStorage.setItem("userName", input.value);
-    const savedName = localStorage.getItem("userName")
+    localStorage.setItem("userName", input.value); //키값과 저장되는 값  파라미터 2개가 필요
+    const savedName = localStorage.getItem("userName"); //읽을때는 키값
     console.log(savedName);
-    h1.innerHTML = savedName;
-
-    
+    h1.innerHTML = "Hello~ " + savedName;
+    loginForm.classList.add("hidden");
 };
 
-
-
 loginForm.addEventListener("submit", callback)
+//submit이벤트를 등록하는 대상은 로그인 버튼이 아닌 폼이다.
+
+
+const savedName = localStorage.getItem("userName");
+if(savedName != null) {
+    h1.innerHTML = "Hello~ " + savedName;
+    loginForm.classList.add("hidden");
+}else {
+    loginForm.classList.remove("hidden");
+}
+
---
+++ 20230321/Counter/app.js
@@ -0,0 +1,63 @@
+const btn = document.querySelector("button");
+const span = document.querySelector("span");
+
+const btnd = document.querySelector(".decrease")
+const btni = document.querySelector(".increase")
+const btnr = document.querySelector(".reset")
+
+
+
+const counter = function(e) {
+    if(e.target.className == "btn decrease") {
+        return span.innerHTML = parseInt(span.innerText) - 1;
+    }else if (e.target.className == "btn increase") {
+        return span.innerHTML = parseInt(span.innerText) + 1;
+    }else {
+        return span.innerHTML = 0;
+    }
+};
+
+btnd.addEventListener("click", counter);
+btni.addEventListener("click", counter);
+btnr.addEventListener("click", counter);
+
+
+const colorChange = function(e) {
+    if(parseInt(span.innerText) < 0) {
+        span.style.color = "red";
+    }else if(parseInt(span.innerText) > 0) {
+        span.style.color = "green";
+    }else {
+        span.style.color = "hsl(209, 61%, 16%)";
+    }
+};
+btnd.addEventListener("click", colorChange);
+btni.addEventListener("click", colorChange);
+btnr.addEventListener("click", colorChange);
+
+
+// let count = 0;
+
+// const span = document.querySelector("span");
+// const btns = document.querySelectorAll(".btn");
+
+// btns.forEach(function(btn) {
+//     btn.addEventListener("click", function(e){
+//         console.log(e);
+//         if (e.target.classList.contains("decrease")) {
+//             count--;
+//         }else if(e.target.classList.contains("increase")) {
+//             count++;
+//         }else {
+//             count = 0;
+//         }
+
+//         if (count > 0) {
+//             span.style.color = "green";
+//         }else if(count < 0) {
+//             span.style.color = "red";
+//         }else {
+//             span.style.color = "#222";
+//         }
+//     })
+// });
